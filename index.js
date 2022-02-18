8f7468ac98f3a0cb82b21c703c80776ab29de8d1
INVISIBLE5130 <goshakovan14@gmail.com>
2022-02-18 11:15:03 +0200

    Added copy url window

--- index.js
+++ index.js
@@ -92,6 +92,97 @@ wrapper.appendChild(wrapperForm)
 document.body.appendChild(wrapper)
 
 window.addEventListener('load', () => {
+    const copyUrlWrapper = document.createElement('div'),
+        copyUrlWrapperTitle = document.createElement('p'),
+        copyUrlWrapperClose = document.createElement('img'),
+        copyUrlWrapperInput = document.createElement('input'),
+        copyUrlWrapperButton = document.createElement('button')
+
+    copyUrlWrapper.style.right = '20px'
+    copyUrlWrapper.style.bottom = '0'
+    copyUrlWrapper.style.position = 'fixed'
+    copyUrlWrapper.style.width = '400px'
+    copyUrlWrapper.style.padding = '20px'
+    copyUrlWrapper.style.display = 'flex'
+    copyUrlWrapper.style.margin = '0 auto auto'
+    copyUrlWrapper.style.borderRadius = '1vh'
+    copyUrlWrapper.style.flexDirection = 'column'
+    copyUrlWrapper.style.backgroundColor = '#375167'
+
+    copyUrlWrapperTitle.style.margin = '0'
+    copyUrlWrapperTitle.style.color = 'white'
+    copyUrlWrapperTitle.style.textAlign = 'center'
+    copyUrlWrapperTitle.style.cursor = 'pointer'
+    copyUrlWrapperTitle.style.fontWeight = 'bold'
+
+    copyUrlWrapperTitle.setAttribute('class', 'show-copy_url-title')
+    copyUrlWrapperTitle.innerHTML = 'Share URL'
+
+    copyUrlWrapperTitle.addEventListener('click', () => {
+        if (copyUrlWrapperTitle.classList.contains('show-copy_url-title')) {
+            copyUrlWrapperTitle.style.display = 'none'
+            copyUrlWrapperClose.style.display = 'block'
+            copyUrlWrapperInput.style.display = 'block'
+            copyUrlWrapperButton.style.display = 'block'
+            copyUrlWrapperTitle.classList.remove('show-copy_url-title')
+        }
+    })
+
+    copyUrlWrapperClose.style.display = 'none'
+    copyUrlWrapperClose.style.width = '20px'
+    copyUrlWrapperClose.style.margin = '0 0 0 auto'
+    copyUrlWrapperClose.style.cursor = 'pointer'
+
+    copyUrlWrapperClose.src = 'https://copointer.com/iframe/img/close.svg'
+    copyUrlWrapperClose.innerHTML = 'X'
+
+    copyUrlWrapperClose.addEventListener('click', () => {
+        if (!copyUrlWrapperTitle.classList.contains('show-copy_url-title')) {
+            copyUrlWrapperTitle.style.display = 'block'
+            copyUrlWrapperClose.style.display = 'none'
+            copyUrlWrapperInput.style.display = 'none'
+            copyUrlWrapperButton.style.display = 'none'
+            copyUrlWrapperTitle.classList.add('show-copy_url-title')
+        }
+    })
+
+    copyUrlWrapperInput.style.display = 'none'
+    copyUrlWrapperInput.style.margin = '1vh 0 0'
+    copyUrlWrapperInput.style.width = inputStyle.width
+    copyUrlWrapperInput.style.padding = inputStyle.padding
+    copyUrlWrapperInput.style.outline = inputStyle.outline
+    copyUrlWrapperInput.style.color = inputStyle.color
+    copyUrlWrapperInput.style.fontSize = inputStyle.fontSize
+    copyUrlWrapperInput.style.lineHeight = inputStyle.lineHeight
+    copyUrlWrapperInput.style.borderRadius = inputStyle.borderRadius
+    copyUrlWrapperInput.style.border = inputStyle.border
+    copyUrlWrapperInput.style.boxSizing = inputStyle.boxSizing
+
+    copyUrlWrapperInput.type = 'text'
+    copyUrlWrapperInput.placeholder = 'Url'
+
+    copyUrlWrapperButton.style.display = 'none'
+    copyUrlWrapperButton.style.width = '100%'
+    copyUrlWrapperButton.style.border = 'none'
+    copyUrlWrapperButton.style.outline = 'none'
+    copyUrlWrapperButton.style.color = '#253746'
+    copyUrlWrapperButton.style.marginTop = '1vh'
+    copyUrlWrapperButton.style.cursor = 'pointer'
+    copyUrlWrapperButton.style.padding = '1.5vh 0'
+    copyUrlWrapperButton.style.fontSize = '16px'
+    copyUrlWrapperButton.style.fontWeight = 'bold'
+    copyUrlWrapperButton.style.borderRadius = '1vh'
+    copyUrlWrapperButton.style.background = '#F2C94C'
+
+    copyUrlWrapperButton.innerHTML = 'Copy URL'
+
+    copyUrlWrapper.appendChild(copyUrlWrapperTitle)
+    copyUrlWrapper.appendChild(copyUrlWrapperClose)
+    copyUrlWrapper.appendChild(copyUrlWrapperInput)
+    copyUrlWrapper.appendChild(copyUrlWrapperButton)
+
+    wrapper.appendChild(copyUrlWrapper)
+
     const keyHandler = e => {
         e.preventDefault()
         if (e.code === 'F1') {
